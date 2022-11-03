8fb5901f8a3363a6733dccf65059358e5a756cf3
withouttheh <lutosibotoboto@gmail.com>
2022-11-03 23:06:21 +0200

    added smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,22 +1,9 @@
-const sliderWeb = document.getElementById('slider-web')
-const sliderData = document.getElementById('slider-data')
-const webBtn = document.getElementById('web-btn')
-const dataBtn = document.getElementById('data-btn')
-
-const changeSlide = () => {
-	console.log('Hi')
-} 
-
-webBtn.addEventListener('click', changeSlide)
-dataBtn.addEventListener('click', changeSlide)
+// SLIDE MENU 
 
 // select dom items
 const menuBtn = document.getElementById("burger");
 
 const menu = document.getElementById("menu");
-
-const topBun = document.getElementById(".top-bun");
-const btmBun = document.getElementById(".bottom-bun");
 
 // Set the initial state of the menu
 let showMenu = false;
@@ -38,4 +25,42 @@ const toggleMenu = () => {
 }
 
 menuBtn.addEventListener("click", toggleMenu);
+menu.addEventListener("click", toggleMenu);
 
+// SCROLL TO ELEMENT
+
+const projects = document.getElementById('projects')
+const projectsBtn = document.getElementById('projects-btn')
+const projectsLink = document.getElementById('projects-link')
+
+const contact = document.getElementById('contact')
+const contactBtn = document.getElementById('contact-btn')
+const contactLink = document.getElementById('contact-link')
+
+const contactScroll = () => {
+	contact.scrollIntoView({ 
+			behavior: 'smooth' 
+		})
+}
+
+const projectsScroll = () => {
+	projects.scrollIntoView({ 
+			behavior: 'smooth' 
+		})
+}
+
+projectsBtn.addEventListener('click', () => {
+	setTimeout(projectsScroll, 500)
+})
+
+projectsLink.addEventListener('click', () => {
+	projectsScroll()
+})
+
+contactBtn.addEventListener('click', () => { 
+	setTimeout(contactScroll, 500)
+})
+
+contactLink.addEventListener('click', () => { 
+	contactScroll()
+})
