be3b83d70e545f48383d7689693ed627a0db31ff
withouttheh <lutosibotoboto@gmail.com>
2022-11-03 12:36:50 +0200

    working nav slider

--- js/main.js
+++ js/main.js
@@ -9,3 +9,33 @@ const changeSlide = () => {
 
 webBtn.addEventListener('click', changeSlide)
 dataBtn.addEventListener('click', changeSlide)
+
+// select dom items
+const menuBtn = document.getElementById("burger");
+
+const menu = document.getElementById("menu");
+
+const topBun = document.getElementById(".top-bun");
+const btmBun = document.getElementById(".bottom-bun");
+
+// Set the initial state of the menu
+let showMenu = false;
+
+const toggleMenu = () => {
+	if (!showMenu) {
+		menuBtn.classList.add("open");
+		menu.classList.add("show");
+
+		// Reset the menu state
+		showMenu = true;
+	} else {
+		menuBtn.classList.remove("open");
+		menu.classList.remove("show");
+
+		// Reset the menu state
+		showMenu = false;
+		}
+}
+
+menuBtn.addEventListener("click", toggleMenu);
+
